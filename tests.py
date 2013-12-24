1cde6c35af584d5d84b1f43db514e312357fa9a0
atOm aNt <julian.romera@gmail.com>
2013-12-24 21:05:40 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python
 
-# This test file aims test a Solr
-# platform (Drupal + Solr in our case).
-# the goail is to provide a realistic 
-# scenario where all the searches dont get
-# retrieved from cache. We do so by generating 
-# a long list of unique words and query the server.
-# At last, the server content has previously 
-# populated with these unique words
-# 
+
 
 
 from random import choice
@@ -17,35 +9,20 @@ import subprocess, os, sys, getopt
 
 def global_consts():
 
-    # set these to a low number and MAX_TESTS
-    # to a hight number, to minimize caches and
-    # get a more real approach 
+
     global MAX_CONNS
     global MAX_CONCURRENT
     
     global HOSTNAME
     global MAX_TESTS
 
-    # settig CONNS to 3 we will get more realistic 
-    # mean/ average values. Also, imho this wont
-    # affect cache hits and therefor, the Solr
-    # performance test
-    
-    # MAX_CONNS value is set to 3. One of those values
-    # comes from a empty cache, the rest 2/3 comes from
-    # primed caches. This will result on a high standard 
-    # deviation on the result, and, in the other side, 
-    # a more realistic value, since the cache system 
-    # is going to be present on that system
+
     MAX_CONNS = '3'
     MAX_CONCURRENT = '1'
-    # include 'http://' and a triling '/' at the end    
+    # include 'http://' and a trailing '/' at the end    
     HOSTNAME = "http://voa3r7.appgee.net/"
     
-    # using here number lower than the words in the
-    # generated dictionary ( about 30000) will
-    # ensure our test that the queryed words are 
-    # not likely cached
+
     MAX_TESTS = 2
     
     
@@ -53,8 +30,6 @@ def global_consts():
 def generate_dictionary():
 
 
-  # how many sufixeds should i create per each root.
-  # e.g: root_elephant1, root_elephant2 <-- 2
   LIMIT_PER_ROOT = 30
 
 
@@ -100,8 +75,7 @@ def generate_dictionary():
     for i in range(1, LIMIT_PER_ROOT):
       DICT.append(root + str(i))
       
-  # 34 items x 30 iteraction = 1400 combinations      
-  # about 1.000 with this setup
+
   print '--> generated '+ str(len(DICT)) +' words in dictionary'      
   return DICT
 
@@ -109,9 +83,7 @@ def generate_dictionary():
 
 def init_settings():
 
-  # non permanent changes
-  # this fixed the "too many open connections error"
-  # of the Apache Bench command
+
   os.system("echo '10240' > /proc/sys/net/core/somaxconn")
   os.system("ulimit -n 65535")
   
@@ -145,7 +117,7 @@ def tests(dict, op):
     mode='horizontal'     
   else:
     # Cache test.
-    # 3 x 400 should be lower (1400) that the total
+    # 3 x 400 should be lower that the total
     # number of words in dictionary, to avoid
     # colisions.
     MAX_CONNS = '3'
